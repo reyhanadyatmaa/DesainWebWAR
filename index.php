867f9bc275289b91359d6b035d5079807bbb2b5a
mhdwahyuuu <mhdwahyuuu@gmail.com>
2019-07-14 19:49:15 +0700

    Halaman Awal dan CSS
    index.php
    style.css
    
    created by wahyu

--- index.php
+++ index.php
@@ -19,16 +19,24 @@
         <li><a href="index.php?page=home">Home</a></li>
         <li class="dropdown"><a href="index.php?page=tentang">DESTINASI</a>
           <ul class="isi-dropdown"> 
-            <li><a href="bali.php">Bali</a></li>
-            <li><a href="lombok.php">Lombok</a></li>
-            <li><a href="sumbar.php">Sumbar </a></li>
-            <li><a href="jogja.php">Yogyakarta</a></li>
+            <li><a href="baju.php">Baju</a></li>
+            <li><a href="sepatu.php">Sepatu</a></li>
+            <li><a href="training.php">Training </a></li>
+            <li><a href="a.php">a</a></li>
           </ul>
         </li>
-        <li><a href="paket.php?page=tutorial">Paket Liburan</a></li>
         <li><a href="tentang.php?page=tutorial">Tentang Kami</a></li>
         <li><a href="Kontak.php?page=tutorial">Kontak</a></li>
       </ul>
     </div>
 
     <div class="badan">
+      <p><h2>SPORT ONLINE OLSHOP</h2></p>
+      <p> Belanja merupakan salah satu kegiatan yang dibutuhkan oleh manusia, mulai dari kebutuhan primer hingga barang-barang hobi. Dulu, kegiatan ini mengharuskan kita keluar rumah.
+
+Bersamaan dengan majunya teknologi, begitu banyak toko online yang mempermudah kegiatan belanja kita. Apalagi, mereka menyediakan aplikasi yang membuat kita lebih nyaman dalam berbelanja online.<br>
+Untuk itu kami hadir untuk membantu anda dalam hal mencari barang-barang olahraga dengan praktis dan harga terjangkau tentunya </p>
+    </div>
+  </div>
+</body>
+</html>
