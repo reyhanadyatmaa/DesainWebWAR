388c350dea012c5c0f124a605da693361c2df00a
mhdwahyuuu <mhdwahyuuu@gmail.com>
2019-07-14 19:16:27 +0700

    Halaman Awal dan CSS
    index.php
    style.css
    
    created by wahyu

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Toko Olshop Olahraga</title>
+  <!-- menghubungkan dengan file css -->
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <!-- menghubungkan dengan file jquery -->
+  <script type="text/javascript" src="jquery.js"></script>
+</head>
+<body>
+  <div class="content">
+    <header>
+      <h1 class="judul">SELAMAT DATANG</h1>
+
+    </header>
+
+    <div class="menu">
+      <ul>
+        <li><a href="index.php?page=home">Home</a></li>
+        <li class="dropdown"><a href="index.php?page=tentang">DESTINASI</a>
+          <ul class="isi-dropdown"> 
+            <li><a href="bali.php">Bali</a></li>
+            <li><a href="lombok.php">Lombok</a></li>
+            <li><a href="sumbar.php">Sumbar </a></li>
+            <li><a href="jogja.php">Yogyakarta</a></li>
+          </ul>
+        </li>
+        <li><a href="paket.php?page=tutorial">Paket Liburan</a></li>
+        <li><a href="tentang.php?page=tutorial">Tentang Kami</a></li>
+        <li><a href="Kontak.php?page=tutorial">Kontak</a></li>
+      </ul>
+    </div>
+
+    <div class="badan">
