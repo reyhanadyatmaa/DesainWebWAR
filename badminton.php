3268cedf116c21fec25446610a7c1f0007057d5b
mhdwahyuuu <mhdwahyuuu@gmail.com>
2019-07-16 01:02:26 +0700

    badminton
    badminton ya kawan

---
+++ badminton.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Badminton</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="content">
+    <header>
+      <h1 class="judul">Badminton</h1>
+      <div class="menu">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="sepakbola.php">Sepak Bola</a></li>
+          <li><a href="bolabasket.php">Bola Basket</a></li>
+          <li><a href="badminton.php">Badminton</a></li>
+          <li><a href="baseball.php">Baseball</a></li>
+          <li><a href="tennis.php">Tennis</a></li>
+          <li><a href="berenang.php">Berenang</a></li>
+          <li><a href="volleyball.php">Volley Ball</a></li>
+          <li><a href="about.php">About</a></li>
+        </ul>
+      </div>
+    </header>
+    
+    <div class="badan">
+      
+      <p><h2><center>BADMINTON</center></h2></p>
+      <img src="img/bulutangkis.jpg" alt="badminton" style="width:500px; height:300px; align-items: center;">
+      <p>Bulu tangkis adalah suatu olahraga rkt yang dimainkan oleh dua orang (untuk tunggal) atau dua pasangan (untuk ganda) yang saling berlawanan. Olah raga yang satu ini hampir mirip seperti tenis. Secara harfiah bulu tangkis berasal dari dua kata, yaitu bulu dan tangkis. Kata bulu diambil dari bentuk atau wujud kok (shuttlecock) yang terbuat dri bulu angsa. Sedangkan “tangkis” diambil dari kata dasar menangkis. Inti dari permainan bulu tangkis adalah menangkis pergerakan dari shuttlecock (bulu) tersebut. Perihal awal mula dan asal permainan bulu tangkis, berbagai pendapat muncul. Pendapat pertama menjelaskan bahwa permainan ini berasal dan berkembang di Mesir kuno pada 2000 tahun yang lalu. Pendapat selanjutnya mengatakan bahwa permainan ini berasal dari India dan daratan Tiongkok. Berkembangnya permainan bulu tangkis ini dikenal luas oleh msyarakat dunia adalah pada abad pertengahan di Inggris, yaitu dari sebuah permainan tradisional anak-anak setempat yang bernama Battledore dan Shuttlecocks. Permainan tradisional ini sangat populer di kalangan masyarakat hingga pada akhirnya sebuah majalah bernama “Punch” mempublikasikan permainan ini. Tujuan permainan bulu tangkis adalah mengumpulkan angka sampai 21. Pemain yang telah lebih dulu mengumpulkan poin atau angka hingga 21 adalah pemenangnya. Pemain bulu tangkis dibedakan menjadi paratai tunggal dan partai ganda. Partai tersebut dapat dimainkan oleh putra maupun putri. Selain itu, partai ganda juga dapat dimainkan secara ganda campuran. Peraturan yang digunakan daalam permainan bulu tangkis harus sesuai dengan peraturan IBF. Kebijakan ini tidak hanya berlaku bagi perlengkapan dan peralatan yang digunakan saja, melainkan partai yang berkepentingan juga harus mengikutinya. Misalnya produsen peralatan.</p>
+    </div>
+  </div>
+</body>
+</html>
