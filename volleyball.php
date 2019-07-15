b4b01c6d367ff1fa642fc9700128f66ec13c9f2f
Madjesty <abghif@gmil.com>
2019-07-16 00:56:53 +0700

    TENNIS

--- volleyball.php
+++ volleyball.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Volley Ball</title>
+  <title>Tennis</title>
   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
   <div class="content">
     <header>
-      <h1 class="judul">VOLLEY BALL</h1>
+      <h1 class="judul">TENNIS</h1>
       <div class="menu">
         <ul>
           <li><a href="index.php">Home</a></li>
@@ -25,9 +25,15 @@
   </div>
   <div class="badan">
     
-    <p><h2><center>TENTANG VOLLEY BALL</center></h2></p>
-    <img src="img/14.jpg" alt="Italian Trulli" style="width:500px; height:300px; align-items: center;">
-    <p>Pada awal penemuannya, olahraga permainan bola voli ini diberi nama Mintonette. Olahraga Mintonette ini pertama kali ditemukan oleh seorang Instruktur pendidikan jasmani (Director of Phsycal Education) yang bernama William G. Morgan di YMCA pada tanggal 9 Februari 1895, di Holyoke, Massachusetts (Amerika Serikat). YMCA (Young Men’s Christian Association) merupakan sebuah organisasi yang didedikasikan untuk mengajarkan ajaran-ajaran pokok umat Kristen kepada para pemuda, seperti yang telah diajarkan oleh Yesus. Organisasi ini didirikan pada tanggal 6 Juni 1884 di London, Inggris oleh George William. Setelah bertemu dengan James Naismith (seorang pencipta olahraga bola basket yang lahir pada tanggal 6 November 1861, dan meninggal pada tanggal 28 November 1939), Morgan menciptakan sebuah olahraga baru yang bernama Mintonette. Sama halnya dengan James Naismith, William G. Morgan juga mendedikasikan hidupnya sebagai seorang instruktur pendidikan jasmani. William G. Morgan yang juga merupakan lulusan Springfield College of YMCA, menciptakan permainan Mintonette ini empat tahun setelah diciptakannya olahraga permainan basketball oleh James Naismith. Olahraga permainan Mintonette sebenarnya merupakan sebuah permainan yang diciptakan dengan mengkombinasikan beberapa jenis permainan. Tepatnya, permainan Mintonette diciptakan dengan mengadopsi empat macam karakter olahraga permainan menjadi satu, yaitu bola basket, baseball, tenis, dan yang terakhir adalah bola tangan (handball). Pada awalnya, permainan ini diciptakan khusus bagi anggota YMCA yang sudah tidak berusia muda lagi, sehingga permainan ini-pun dibuat tidak seaktif permainan bola basket.</p>
+    <p><h2><center>ABOUT TENNIS</center></h2></p>
+    <img src="img/15.jpg" alt="Italian Trulli" style="width:500px; height:300px; align-items: center;">
+    <p>Tenis adalah olahraga yang biasanya dimainkan antara dua pemain atau antara dua pasangan masing-masing dua pemain. Setiap pemain menggunakan raket untuk memukul bola karet. Tujuan permainan adalah memainkan bola dengan cara tertentu sehingga pemain lawan tidak dapat mengembalikan bola tersebut.
+
+Tenis adalah salah satu cabang olahraga Olimpiade dan dimainkan pada semua tingkat masyarakat di segala usia. Olahraga ini dapat dimainkan oleh siapa saja, termasuk orang-orang yang menggunakan kursi roda. Permainan tenis modern berasal dari Birmingham, Inggris pada akhir abad ke-19 sebagai "tenis lapangan rumput".[1]
+
+Peraturan tenis berubah sedikit sejak 1890-an. Dua perubahan kecil adalah sejak 1908 hingga 1961 pemain yang melakukan service (pukulan pertama) harus menjaga salah satu kakinya tetap di tanah hingga service berpindah dan adopsi sistem tie-break pada 1970-an. Tambahan terakhir yang diterapkan pada tenis profesional adalah teknologi tinjauan ulang elektronik.
+
+Tenis dimainkan oleh jutaan orang sebagai olahraga rekreasi dan juga merupakan olahraga tontontan populer di seluruh dunia. Empat kejuaraan tenis terkemuka adalah Australia Terbuka yang dimainkan di lapangan keras, Prancis Terbuka yang dimainkan di lapangan tanah liat, Wimbledon yang dimainkan di lapangan rumput, dan AS Terbuka yang dimainkan juga di lapangan keras.</p>
   </div>
 </body>
 </html>
