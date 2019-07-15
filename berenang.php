6c991e13850959240db805285803efcd0de606ac
mhdwahyuuu <mhdwahyuuu@gmail.com>
2019-07-16 01:08:13 +0700

    berenang
    berenang

---
+++ berenang.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Berenang</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="content">
+    <header>
+      <h1 class="judul">Berenang</h1>
+      <div class="menu">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="sepakbola.php">Sepak Bola</a></li>
+          <li><a href="bolabasket.php">Bola Basket</a></li>
+          <li><a href="badminton.php">Badminton</a></li>
+          <li><a href="baseball.php">Baseball</a></li>
+          <li><a href="tennis.php">Tennis</a></li>
+          <li><a href="berenang.php">Berenang</a></li>
+          <li><a href="volleyball.php">Volley Ball</a></li>
+          <li><a href="about.php">About</a></li>
+        </ul>
+      </div>
+    </header>
+    
+    <div class="badan">
+      
+      <p><h2><center>Berenang</center></h2></p>
+      <img src="img/berenang.jpg" alt="berenang" style="width:500px; height:300px; align-items: center;">
+      <p>Berenang termasuk salah satu olahraga menyehatkan karena seluruh anggota tubuh bergerak serta melatih pernafasan. Kegiatan berenang bisa bertujuan untuk olahraga maupun sekedar rekreasi guna melepas penat. Saat ini berenang sudah dikompetisikan baik secara nasional maupun internasional. Namun sebenarnya bagaimana sejarah renang sampai menjadi olahraga yang populer? Tahukah anda? Renang merupakan jenis olahraga yang melombakan kecepatan sang atlet dalam berenang. Mengenai peraturan perlombaan renang sudah ditetapkan oleh badan dunia yang bernama FINA. Sedangkan PRSI (Persatuan Renang Seluruh Indonesia) merupakan induk organisasi dari cabang olahraga renang di Indonesia. Kegiatan berenang ini ada yang dilakukan di kolam renang indoor maupun outdoor. Gaya renang yang sering  diperlombakan adalah gaya dada, gaya bebas, gaya kupu-kupu, dan gaya punggung. Perenang yang dinyatakan menang dalam perlombaan adalah yang mampu menyelesaikan jarak lintasan paling cepat. Jika menang dalam babak penyisihan akan maju ke babak semifinal baru setelah itu maju ke babak final.</p>
+    </div>
+  </div>
+</body>
+</html>
