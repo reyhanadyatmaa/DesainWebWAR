05a802af0fc925d7155166e83b389be6237dabfe
reyhan adyatma <reyhanadyatmaa1503@gmail.com>
2019-07-16 12:39:36 +0700

    uyu

--- bolabasket.php
+++ bolabasket.php
@@ -1,43 +1,52 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Olahraga</title>
+  <title>Bola Basket</title>
   <link rel="stylesheet" type="text/css" href="style.css">
+  <script type="text/javascript" src="jquery.js"></script>
 </head>
+
 <body>
-  <div class="content">
-    <header>
-      <h1 class="judul">OLAHRAGA</h1>
-      <div class="menu">
+
+<div class="content">
+  <header>
+    <h1 class="judul"><center>BOLA BASKET</center></h1>
+    
+  </header>
+
+  <div class="menu">
         <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="sepakbola.php">Sepak Bola</a></li>
-          <li><a href="bolabasket.php">Bola Basket</a></li>
-          <li><a href="badminton.php">Badminton</a></li>
-          <li><a href="baseball.php">Baseball</a></li>
-          <li><a href="tennis.php">Tennis</a></li>
-          <li><a href="berenang.php">Berenang</a></li>
-          <li><a href="volleyball.php">Volley Ball</a></li>
-          <li><a href="about.php">About</a></li>
+          <li ><a href="index.php">Home</a></li>
+          <li ><a href="sepakbola.php">Sepak Bola</a></li>
+          <li ><a href="bolabasket.php">Bola Basket</a></li>
+          <li ><a href="badminton.php">Badminton</a></li>
+          <li ><a href="baseball.php">Baseball</a></li>
+          <li ><a href="tennis.php">Tennis</a></li>
+          <li ><a href="berenang.php">Berenang</a></li>
+          <li ><a href="volleyball.php">Volley Ball</a></li>
+          <li ><a href="about.php">About</a></li>
         </ul>
       </div>
     </header>
-  </div>
-  <div class="badan">
-    
-    <p><h2><center>BASKET</center></h2></p>
-    <img src="img/1.jpg" alt="Italian Trulli" style="width:500px; height:300px; align-items: center;">
-    <p>Basket adalah olahraga bola berkelompok yang terdiri atas dua tim beranggotakan masing-masing lima orang yang saling bertanding mencetak poin dengan memasukkan bola ke dalam keranjang lawan. Bola basket sangat cocok untuk ditonton karena biasa dimainkan di ruang olahraga tertutup dan hanya memerlukan lapangan yang relatif kecil. Selain itu, permainan bola basket juga lebih kompetitif karena tempo permainan cenderung lebih cepat jika dibandingkan dengan olahraga bola yang lain, seperti voli dan sepak bola. Ada 3 posisi utama dalam bermain basket, yaitu: 1) Forward, pemain yang tugas utamanya adalah mencetak poin dengan memasukkan bola ke keranjang lawan, 2) Defense, pemain yang tugas utamanya adalah menjaga pemain lawan agar pemain lawan kesulitan memasukkan bola, dan 3) Playmaker, pemain yang menjadi tokoh kunci permainan dengan mengatur alur bola dan strategi yang dimainkan oleh rekan-rekan setimnya.
 
-Bola basket adalah salah satu olahraga yang paling digemari oleh penduduk Amerika Serikat dan penduduk di belahan bumi lainnya, antara lain di Amerika Selatan, Eropa Selatan, Lithuania, dan juga di Indonesia. Banyak kompetisi bola basket yang diselenggarakan setiap tahun, seperti British Basketball League (BBL) di Inggris, National Basketball Association (NBA) di Amerika, dan Indonesia Basketball League (IBL) di Indonesia..</p>
+    <div class="badan">
+      <style >
+      .tengah{
+        margin-left:auto;
+        margin-right:auto;
+        display:block;
+        width:200px
+        }
+        p{
+          text-align: center;
+        }
+      </style>
+      <p><h2><center>BASKET</center></h2></p>
+      <img class="tengah" src="img/1.jpg" alt="Bola Basket" style="width:500px; height:300px;">
+      <p>Basket adalah olahraga bola berkelompok yang terdiri atas dua tim beranggotakan masing-masing lima orang yang saling bertanding mencetak poin dengan memasukkan bola ke dalam keranjang lawan. Bola basket sangat cocok untuk ditonton karena biasa dimainkan di ruang olahraga tertutup dan hanya memerlukan lapangan yang relatif kecil. Selain itu, permainan bola basket juga lebih kompetitif karena tempo permainan cenderung lebih cepat jika dibandingkan dengan olahraga bola yang lain, seperti voli dan sepak bola. Ada 3 posisi utama dalam bermain basket, yaitu: 1) Forward, pemain yang tugas utamanya adalah mencetak poin dengan memasukkan bola ke keranjang lawan, 2) Defense, pemain yang tugas utamanya adalah menjaga pemain lawan agar pemain lawan kesulitan memasukkan bola, dan 3) Playmaker, pemain yang menjadi tokoh kunci permainan dengan mengatur alur bola dan strategi yang dimainkan oleh rekan-rekan setimnya. Bola basket adalah salah satu olahraga yang paling digemari oleh penduduk Amerika Serikat dan penduduk di belahan bumi lainnya, antara lain di Amerika Selatan, Eropa Selatan, Lithuania, dan juga di Indonesia. Banyak kompetisi bola basket yang diselenggarakan setiap tahun, seperti British Basketball League (BBL) di Inggris, National Basketball Association (NBA) di Amerika, dan Indonesia Basketball League (IBL) di Indonesia..</p>
 
-<p>Basket dianggap sebagai olahraga unik karena diciptakan secara tidak sengaja oleh seorang guru olahraga. Pada tahun 1891, Dr. James Naismith, seorang guru olahraga asal Kanada yang mengajar di sebuah perguruan tinggi untuk para siswa profesional di YMCA (sebuah wadah pemuda umat Kristen) di Springfield, Massachusetts, harus membuat suatu permainan di ruang tertutup untuk mengisi waktu para siswa pada masa liburan musim dingin di New England. Terinspirasi dari permainan yang pernah ia mainkan saat kecil di Ontario, Naismith menciptakan permainan yang sekarang dikenal sebagai bola basket pada 15 Desember 1891.
-
-Menurut cerita, setelah menolak beberapa gagasan karena dianggap terlalu keras dan kurang cocok untuk dimainkan di gelanggang-gelanggang tertutup, dia lalu menulis beberapa peraturan dasar, menempelkan sebuah keranjang di dinding ruang gelanggang olahraga, dan meminta para siswa untuk mulai memainkan permainan ciptaannya itu.
-
-Pertandingan resmi bola basket yang pertama, diselenggarakan pada tanggal 20 Januari 1892 di tempat kerja Dr.James Naismith. Basket adalah sebutan yang diucapkan oleh salah seorang muridnya. Olahraga ini pun segera terkenal di seantero Amerika Serikat. Penggemar fanatik ditempatkan di seluruh cabang di Amerika Serikat. Pertandingan demi pertandingan pun dilaksanakan di seluruh kota-kota negara bagian Amerika Serikat.
-
-Pada awalnya, setiap tim berjumlah sembilan orang dan tidak ada dribble, sehingga bola hanya dapat berpindah melalui lemparan. Sejarah peraturan permainan basket diawali dari 13 aturan dasar yang ditulis sendiri oleh James Naismith.</p>
+      <p>Basket dianggap sebagai olahraga unik karena diciptakan secara tidak sengaja oleh seorang guru olahraga. Pada tahun 1891, Dr. James Naismith, seorang guru olahraga asal Kanada yang mengajar di sebuah perguruan tinggi untuk para siswa profesional di YMCA (sebuah wadah pemuda umat Kristen) di Springfield, Massachusetts, harus membuat suatu permainan di ruang tertutup untuk mengisi waktu para siswa pada masa liburan musim dingin di New England. Terinspirasi dari permainan yang pernah ia mainkan saat kecil di Ontario, Naismith menciptakan permainan yang sekarang dikenal sebagai bola basket pada 15 Desember 1891. Menurut cerita, setelah menolak beberapa gagasan karena dianggap terlalu keras dan kurang cocok untuk dimainkan di gelanggang-gelanggang tertutup, dia lalu menulis beberapa peraturan dasar, menempelkan sebuah keranjang di dinding ruang gelanggang olahraga, dan meminta para siswa untuk mulai memainkan permainan ciptaannya itu. Pertandingan resmi bola basket yang pertama, diselenggarakan pada tanggal 20 Januari 1892 di tempat kerja Dr.James Naismith. Basket adalah sebutan yang diucapkan oleh salah seorang muridnya. Olahraga ini pun segera terkenal di seantero Amerika Serikat. Penggemar fanatik ditempatkan di seluruh cabang di Amerika Serikat. Pertandingan demi pertandingan pun dilaksanakan di seluruh kota-kota negara bagian Amerika Serikat. Pada awalnya, setiap tim berjumlah sembilan orang dan tidak ada dribble, sehingga bola hanya dapat berpindah melalui lemparan. Sejarah peraturan permainan basket diawali dari 13 aturan dasar yang ditulis sendiri oleh James Naismith.</p>
+    </div>
   </div>
 </body>
 </html>
--- sepakbola.php
+++ sepakbola.php
@@ -1,39 +1,56 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Olahraga</title>
+  <title>SEPAKBOLA</title>
   <link rel="stylesheet" type="text/css" href="style.css">
+  <script type="text/javascript" src="jquery.js"></script>
 </head>
+
 <body>
-  <div class="content">
-    <header>
-      <h1 class="judul">OLAHRAGA</h1>
-      <div class="menu">
+
+<div class="content">
+  <header>
+    <h1 class="judul"><center>SEPAK BOLA</center></h1>
+    
+  </header>
+
+  <div class="menu">
         <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="sepakbola.php">Sepak Bola</a></li>
-          <li><a href="bolabasket.php">Bola Basket</a></li>
-          <li><a href="badminton.php">Badminton</a></li>
-          <li><a href="baseball.php">Baseball</a></li>
-          <li><a href="tennis.php">Tennis</a></li>
-          <li><a href="berenang.php">Berenang</a></li>
-          <li><a href="volleyball.php">Volley Ball</a></li>
-          <li><a href="about.php">About</a></li>
+          <li ><a href="index.php">Home</a></li>
+          <li ><a href="sepakbola.php">Sepak Bola</a></li>
+          <li ><a href="bolabasket.php">Bola Basket</a></li>
+          <li ><a href="badminton.php">Badminton</a></li>
+          <li ><a href="baseball.php">Baseball</a></li>
+          <li ><a href="tennis.php">Tennis</a></li>
+          <li ><a href="berenang.php">Berenang</a></li>
+          <li ><a href="volleyball.php">Volley Ball</a></li>
+          <li ><a href="about.php">About</a></li>
         </ul>
       </div>
     </header>
-  </div>
-  <div class="badan">
-    
-    <p><h2><center>SEPAKBOLA</center></h2></p>
-    <img src="img/12.jpg" alt="Italian Trulli" style="width:500px; height:300px; align-items: center;">
-    <p>Sepak bola adalah cabang olahraga yang memakai sebuah bola, biasanya berbahan kulit atau karet dan dimainkan oleh dua tim, dimana masing-masing tim beranggotakan 11 pemain inti dan beberapa pemain pengganti.
+
+    <div class="badan">
+      <style >
+      .tengah{
+        margin-left:auto;
+        margin-right:auto;
+        display:block;
+        width:200px
+        }
+        p{
+          text-align: center;
+        }
+      </style>
+      <p><h2><center>SEPAK BOLA</center></h2></p>
+      <img class="tengah" src="img/12.jpg" alt="Italian Trulli" style="width:500px; height:300px;">
+      <p>Sepak bola adalah cabang olahraga yang memakai sebuah bola, biasanya berbahan kulit atau karet dan dimainkan oleh dua tim, dimana masing-masing tim beranggotakan 11 pemain inti dan beberapa pemain pengganti.
 
 Selain itu sepak bola juga bisa didefinisikan sebagai suatu permainan yang dilakukan dengan cara menendang bola, yang tujuannya adalah untuk memasukkan bola terebut ke gawang lawan. Suatu permainan sepak bola dimenangkan oleh tim yang paling banyak memasukkan bola ke lawan berdasarkan aturan permainan yang berlaku.
 
 Sepak bola dalam bahasa Inggris disebut dengan istilah ‘football’. Sedangkan di Amerika Serikat disebut dengan julukan ‘soccer’. Bisa kita amati bahwa sepak bola terdiri dari dua kata, yakni sepak dan bola. Sepak memiliki arti menendang dengan kaki. Sedangkan bola adalah alat permainan yang berbentuk bulat dan biasanya terbuat dari bahan kulit atau karet.Sepak bola menjadi salah satu jenis olahraga yang banyak diminati orang, terutama kaum adam. Tak sulit untuk mencari orang-orang yang bermain sepak bola di tanah lapang. Biasanya banyak anak-anak bermain sepak bola saat sore hari atau ketika hari libur.
 
 Tidak hanya itu, pertandingan sepak bola juga sering diselenggarakan di Indonesia ataupun tingkat dunia. Dengan antusiasme masyarakat yang cukup besar..</p>
+    </div>
   </div>
 </body>
 </html>
