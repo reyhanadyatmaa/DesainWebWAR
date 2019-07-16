70d9679868b4d7e99f088b2a7773aa5f57eecc53
mhdwahyuuu <mhdwahyuuu@gmail.com>
2019-07-16 13:19:22 +0700

    About Us dan Gambar

---
+++ about.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>About Us</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script type="text/javascript" src="jquery.js"></script>
+</head>
+
+<body>
+
+  <div class="content">
+    <header>
+      <h1 class="judul"><center>About Us</center></h1>
+
+    </header>
+
+    <div class="menu">
+      <ul>
+        <li ><a href="index.php">Home</a></li>
+        <li ><a href="sepakbola.php">Sepak Bola</a></li>
+        <li ><a href="bolabasket.php">Bola Basket</a></li>
+        <li ><a href="badminton.php">Badminton</a></li>
+        <li ><a href="baseball.php">Baseball</a></li>
+        <li ><a href="tennis.php">Tennis</a></li>
+        <li ><a href="berenang.php">Berenang</a></li>
+        <li ><a href="volleyball.php">Volley Ball</a></li>
+        <li ><a href="about.php">About</a></li>
+      </ul>
+    </div>
+  </header>
+
+  <div class="badan">
+    <style >
+    .tengah{
+      margin-left:auto;
+      margin-right:auto;
+      display:block;
+      width:200px
+    }
+    p{
+      text-align: center;
+    }
+  </style>
+  <p><h2><center>Sang Calon Programmer :)</center></h2></p>
+  <img class="tengah" src="img/calon_programmer.jpg" alt="Abghi, Reyhan, Wahyu" style="width:500px; height:300px;">
+  <h3><center>Kami bertiga merupakan Programer yang awalnya tidak menduga akan bisa membuat hasil yang brilian seperti web ini, yang pertama mungkin kami hanya berterima kasih kepada allah swt mungkin kami akan membuat yang lebih besar dari pada ini lagi, dan kami akan terus membuat olahraga menjadi pusat untuk menjadi kebanggan kita semua,kami bertiga pamit untuk projek besar berikutnya<center></h3>
+</div>
+</div>
+</body>
+</html>
