33480c289b35a15b6e19e2618766599091410534
mhdwahyuuu <mhdwahyuuu@gmail.com>
2019-07-16 12:31:38 +0700

    index dan css
    Update index dan style css

--- index.php
+++ index.php
@@ -3,30 +3,46 @@
 <head>
   <title>Olahraga</title>
   <link rel="stylesheet" type="text/css" href="style.css">
+  <script type="text/javascript" src="jquery.js"></script>
 </head>
+
 <body>
-  <div class="content">
-    <header>
-      <h1 class="judul">OLAHRAGA</h1>
-      <div class="menu">
+
+<div class="content">
+  <header>
+    <h1 class="judul"><center>OLAHRAGA</center></h1>
+    
+  </header>
+
+  <div class="menu">
         <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="sepakbola.php">Sepak Bola</a></li>
-          <li><a href="bolabasket.php">Bola Basket</a></li>
-          <li><a href="badminton.php">Badminton</a></li>
-          <li><a href="baseball.php">Baseball</a></li>
-          <li><a href="tennis.php">Tennis</a></li>
-          <li><a href="berenang.php">Berenang</a></li>
-          <li><a href="volleyball.php">Volley Ball</a></li>
-          <li><a href="about.php">About</a></li>
+          <li ><a href="index.php">Home</a></li>
+          <li ><a href="sepakbola.php">Sepak Bola</a></li>
+          <li ><a href="bolabasket.php">Bola Basket</a></li>
+          <li ><a href="badminton.php">Badminton</a></li>
+          <li ><a href="baseball.php">Baseball</a></li>
+          <li ><a href="tennis.php">Tennis</a></li>
+          <li ><a href="berenang.php">Berenang</a></li>
+          <li ><a href="volleyball.php">Volley Ball</a></li>
+          <li ><a href="about.php">About</a></li>
         </ul>
       </div>
     </header>
-    
+
     <div class="badan">
-      
+      <style >
+      .tengah{
+        margin-left:auto;
+        margin-right:auto;
+        display:block;
+        width:200px
+        }
+        p{
+          text-align: center;
+        }
+      </style>
       <p><h2><center>PENTINGNYA OLAHRAGA</center></h2></p>
-      <img src="img/olahraga.jpg" alt="Italian Trulli" style="width:500px; height:300px; align-items: center;">
+      <img class="tengah" src="img/olahraga.jpg" alt="Italian Trulli" style="width:500px; height:300px;">
       <p>Olahraga merupakan aktivitas yang memiliki tujuan tertentu, seperti halnya melatih tubuh untuk kesehatan jasmani maupun rohani. Sehingga olahraga yang rutin dapat memiliki manfaat untuk menjaga kesehatan tubuh. Dikarenakan metabolisme dalam tubuh dapat berjalan lancar. Dengan begitu, penyerapan serta distribusi nutrisi dapat bekerja dengan efektif dan efisien. Sehingga pengertian Olahraga secara umum adalah suatu bentuk aktivitas fisik yang terencana dan terstruktur, yang dimana melibatkan gerakan tubuh secara berulang-ulang demi mendapatkan hasil yang baik. Dengan tujuan sebagai peningkatan kebugaran jasmani maupun rohani tiap manusia. Olahraga juga bisa dilakukan oleh orang dewasa, anak-anak, hingga lanjut usia selagi dia mampu. Olahraga juga telah menjadi bagian hidup dari sebagian masyarakat perkotaan maupun pedesaan. Yang dilihat dari Kebiasaan gaya hidup seseorang yang suka merokok. Berolahraga dengan teratur, ternyata sangat berpeluang untuk terhindar dari penyakit strok dan jantung. Tidak hanya itu, segala penyakit juga bisa dihindari dengan teraturnya berolahraga.</p>
     </div>
   </div>
