ab9b6e76d67325e83b58ca342ce7475d7c60dd6c
mhdwahyuuu <mhdwahyuuu@gmail.com>
2019-07-16 12:38:58 +0700

    Update index.php
    index ada tambahan manfaatnya

--- index.php
+++ index.php
@@ -44,6 +44,35 @@
       <p><h2><center>PENTINGNYA OLAHRAGA</center></h2></p>
       <img class="tengah" src="img/olahraga.jpg" alt="Italian Trulli" style="width:500px; height:300px;">
       <p>Olahraga merupakan aktivitas yang memiliki tujuan tertentu, seperti halnya melatih tubuh untuk kesehatan jasmani maupun rohani. Sehingga olahraga yang rutin dapat memiliki manfaat untuk menjaga kesehatan tubuh. Dikarenakan metabolisme dalam tubuh dapat berjalan lancar. Dengan begitu, penyerapan serta distribusi nutrisi dapat bekerja dengan efektif dan efisien. Sehingga pengertian Olahraga secara umum adalah suatu bentuk aktivitas fisik yang terencana dan terstruktur, yang dimana melibatkan gerakan tubuh secara berulang-ulang demi mendapatkan hasil yang baik. Dengan tujuan sebagai peningkatan kebugaran jasmani maupun rohani tiap manusia. Olahraga juga bisa dilakukan oleh orang dewasa, anak-anak, hingga lanjut usia selagi dia mampu. Olahraga juga telah menjadi bagian hidup dari sebagian masyarakat perkotaan maupun pedesaan. Yang dilihat dari Kebiasaan gaya hidup seseorang yang suka merokok. Berolahraga dengan teratur, ternyata sangat berpeluang untuk terhindar dari penyakit strok dan jantung. Tidak hanya itu, segala penyakit juga bisa dihindari dengan teraturnya berolahraga.</p>
+
+      <p>Manfaat Dan Tujuan Olahraga
+Olahraga juga memiliki manfaat bagi seseorang. Manfaat olahraga tersebut yang mampu membantu seseorang untuk lebih percaya diri, memperlancar peredaran darah, meningkatkan kinerja otak secara maksimal untuk mendapat hasil konsentrasi yang baik. Karena suplai oksigen menuju otak dapat beredar secara lancar.
+
+Selanjutnya, tujuan dari Olahraga sendiri sangat beragam. Hingga mengeluarkan berbagai pengertian olahraga yang sangat luas. Serta banyak yang menganggap bahwa olahraga hanya sebagai hobi, mempercepat proses diet, atau keinginan membentuk otot tubuh. Dan juga sekedar bermain. Namun, olahraga sendiri akan berbuah manis jika seseorang melakukannya dengan rutin dan dalam terarah untuk mengikuti PORKAB, PORPROV, PON, SEA GAMES, dan lain sebagainya.
+
+Macam-Macam Cabang Olahraga Olimpiade
+Aktivitas olahraga seharusnya menjadi hal yang wajib dilakukan. Karena pengertian olahraga itu sendiri adalah sebagai suatu aktivitas yang menyehatkan tubuh untuk selalu sehat dan aktif. Namun, bagaimana jika ada beberapa olahraga yang memiliki cabang olimpiade. Bukankah itu akan menjadi kehebatan tersendiri? Berikut ada beberapa cabor yang dipertandingkan dibawah ini :
+
+1. Atletik
+Olahraga ini merupakan olahraga gabungan dari berbagai jenis olahraga yang dikelompokkan menjadi satu. Yaitu olahraga lari, lompat dan lempar. Contoh olahraga atletik sendiri adalah lari jarah pendek, lari jarak menengah, lari maraton, tolak peluru, lempar lembing, lompat tinggi dan masih banyak lagi.
+
+2. Permainan Bola
+Untuk permainan bola yang di Olimpiadekan adalah permainan bola besar dan bola kecil. Permainan bola besar adalah bola basket, bola voli, futsal, golf, sofbol, tennis lapangan, boling, sepak bola, bola tangan dan lain sebagainya.
+
+  Pengertian Budaya Politik
+Permain bola, pada era ini tidak hanya digemari oleh kaum laki-laki saja. Namun, kaum perempuan pun juga banyak yang gemar bermain bola. Contoh bola basket, bola volli, bola tangan, tennis lapangan adalah contoh olahraga yang digemari oleh kaum perempuan. Hingga kini banyak perempuan yang menjuarai olahraga bola tersebut.
+
+3. Seni Bela Diri
+Cabang olahraga seni beladiri kini semakin menjadi idola baru dalam cabang Olahraga. Kemunculan untuk mempertahankan nilai seni dari berbagai daerah bisa berkembang dengan aktivitas dari adanya olahraga bela diri. Dari masa ke masa, seni bela diri tersebar ke seluruh penjuru dunia dan memiliki ciri khas masing-masing.
+
+Seperti halnya silat, bela diri silat adalah olahraga yang dikembangkan di Indonesia. Olahraga yang di perlombakan dalam tingkat Olimpiade ini harus berkembang dan bersaing secara positif bersama seni beladiri aikido, judo, wushu, karate, gulat, kempo, wing tsun, dan taekwondo.
+
+4. Berenang
+Olahraga air ini, merupakan cabang olahraga yang dilakukan di dalam air. Kompetisi ini selalu dimasukkan dalam segala pertandingan. Kelas renang yang biasa diperlombakan yaitu gaya dada, gaya bebas, gaya punggung, dan gaya kupu-kupu. Nantinya olahraga ini akan diperlombakan secara acak, sesuai dengan kecepatan waktu yang di tempuh.
+
+Itulah macam-macam cabang Olahraga Olimpiade yang dipertandingkan pada setiap kejuaraan yang ada. Dengan dibantunya pengurus pada setiap cabornya (cabang olahraga) membantu seseorang untuk memilih bidang olahraga yang tepat dengan kemampuan yang dimiliki masing-masing manusia
+
+Nah, itulah penjelasan dari pengertian olahraga, manfaat, tujuan hingga macam-macam olahraga yang masuk dalam tingkat Olimpiade yang dipertandingkan. Semoga ini bermanfaat dan bisa dijadikan motivasi sebagai giatnya olahraga menuju prestasi tertinggi.</p>
     </div>
   </div>
 </body>
